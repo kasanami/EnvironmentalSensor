a20374d121c8733f1f1b53b675bc3db7814eca34
笠浪貴大 <dev.kacha@gmail.com>
2019-09-01 19:24:50 +0900

    デモアプリプロジェクト追加

---
+++ EnvironmentalSensor/DemoApp/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace DemoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.portsComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.stateLlabel = new System.Windows.Forms.Label();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.latestDataLongGetButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tableLayoutPanel2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(794, 64);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "接続";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 7;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.disconnectButton, 5, 0);
+            this.tableLayoutPanel2.Controls.Add(this.connectButton, 4, 0);
+            this.tableLayoutPanel2.Controls.Add(this.stateLlabel, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.portsComboBox, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 15);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(788, 46);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ポート";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // portsComboBox
+            // 
+            this.portsComboBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.portsComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.portsComboBox.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.portsComboBox.FormattingEnabled = true;
+            this.portsComboBox.Location = new System.Drawing.Point(103, 3);
+            this.portsComboBox.Name = "portsComboBox";
+            this.portsComboBox.Size = new System.Drawing.Size(94, 32);
+            this.portsComboBox.TabIndex = 2;
+            this.portsComboBox.SelectedIndexChanged += new System.EventHandler(this.PortsComboBox_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(203, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 40);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "状態";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // stateLlabel
+            // 
+            this.stateLlabel.AutoSize = true;
+            this.stateLlabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.stateLlabel.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.stateLlabel.Location = new System.Drawing.Point(303, 0);
+            this.stateLlabel.Name = "stateLlabel";
+            this.stateLlabel.Size = new System.Drawing.Size(94, 40);
+            this.stateLlabel.TabIndex = 4;
+            this.stateLlabel.Text = "-";
+            this.stateLlabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // connectButton
+            // 
+            this.connectButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.connectButton.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.connectButton.Location = new System.Drawing.Point(403, 3);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(94, 34);
+            this.connectButton.TabIndex = 5;
+            this.connectButton.Text = "接続";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.disconnectButton.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.disconnectButton.Location = new System.Drawing.Point(503, 3);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(94, 34);
+            this.disconnectButton.TabIndex = 6;
+            this.disconnectButton.Text = "切断";
+            this.disconnectButton.UseVisualStyleBackColor = true;
+            this.disconnectButton.Click += new System.EventHandler(this.DisconnectButton_Click);
+            // 
+            // serialPort
+            // 
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.SerialPort_DataReceived);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tableLayoutPanel3);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(3, 73);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(794, 64);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "制御";
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.latestDataLongGetButton, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 15);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(788, 46);
+            this.tableLayoutPanel3.TabIndex = 0;
+            // 
+            // latestDataLongGetButton
+            // 
+            this.latestDataLongGetButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.latestDataLongGetButton.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.latestDataLongGetButton.Location = new System.Drawing.Point(3, 3);
+            this.latestDataLongGetButton.Name = "latestDataLongGetButton";
+            this.latestDataLongGetButton.Size = new System.Drawing.Size(388, 34);
+            this.latestDataLongGetButton.TabIndex = 4;
+            this.latestDataLongGetButton.Text = "最新データ取得";
+            this.latestDataLongGetButton.UseVisualStyleBackColor = true;
+            this.latestDataLongGetButton.Click += new System.EventHandler(this.LatestDataLongGetButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "DemoApp";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button disconnectButton;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Label stateLlabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox portsComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button latestDataLongGetButton;
+    }
+}
+
---
+++ EnvironmentalSensor/DemoApp/Form1.cs
@@ -0,0 +1,132 @@
+﻿using EnvironmentalSensor.USB;
+using EnvironmentalSensor.USB.Payloads;
+using Ksnm.ExtensionMethods.System.Collections.Generic.Enumerable;
+using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace DemoApp
+{
+    public partial class Form1 : Form
+    {
+
+        string selectedPort = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // すべてのシリアル・ポート名を取得する
+            var ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                portsComboBox.Items.Add(port);
+            }
+            if (ports.Length > 0)
+            {
+                portsComboBox.SelectedIndex = 0;
+                selectedPort = ports[0];
+            }
+            UpdateUI();
+        }
+
+        void UpdateUI()
+        {
+            if (serialPort.IsOpen)
+            {
+                stateLlabel.Text = "接続中";
+                connectButton.Enabled = false;
+                disconnectButton.Enabled = true;
+                latestDataLongGetButton.Enabled = true;
+            }
+            else
+            {
+                stateLlabel.Text = "非接続";
+                connectButton.Enabled = true;
+                disconnectButton.Enabled = false;
+                latestDataLongGetButton.Enabled = false;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            serialPort.Close();
+        }
+
+        private void PortsComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedPort = (string)portsComboBox.SelectedItem;
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen == false && selectedPort != null)
+            {
+                serialPort.BaudRate = 115200;
+                serialPort.DataBits = 8;
+                serialPort.StopBits = StopBits.One;
+                serialPort.Parity = Parity.None;
+                serialPort.Handshake = Handshake.None;// フロー制御：None
+                serialPort.PortName = selectedPort;
+                serialPort.Open();
+            }
+            UpdateUI();
+        }
+
+        private void DisconnectButton_Click(object sender, EventArgs e)
+        {
+            serialPort.Close();
+            UpdateUI();
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            byte[] buffer = new byte[1024];
+            var readSize = serialPort.Read(buffer, 0, buffer.Length);
+            Console.WriteLine("DataReceived");
+            Console.WriteLine($"readSize={readSize}");
+            if (readSize > 0)
+            {
+                Console.WriteLine($"buffer={buffer.ToDebugString()}");
+                try
+                {
+                    var frame = new Frame(buffer, 0, readSize);
+
+                    if (frame.Payload is ErrorResponsePayload)
+                    {
+                        var payload = frame.Payload as ErrorResponsePayload;
+                    }
+                    else if (frame.Payload is MemoryDataLongResponsePayload)
+                    {
+                        var payload = frame.Payload as MemoryDataLongResponsePayload;
+                    }
+                    else if (frame.Payload is LatestDataLongResponsePayload)
+                    {
+                        var payload = frame.Payload as LatestDataLongResponsePayload;
+                    }
+                    Console.WriteLine(frame.Payload.ToString());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private void LatestDataLongGetButton_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                var payload = new LatestDataLongCommandPayload();
+                var frame = new Frame(payload);
+                var buffer = frame.ToBytes();
+                Console.WriteLine(buffer.ToDebugString());
+                serialPort.Write(buffer, 0, buffer.Length);
+            }
+            else
+            {
+                Console.WriteLine("閉じてる");
+            }
+        }
+    }
+}
