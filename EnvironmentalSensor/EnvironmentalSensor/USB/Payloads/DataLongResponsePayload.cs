1cc6c650744643e5e637938b1e09375a27033cb6
笠浪貴大 <dev.kacha@gmail.com>
2019-09-05 02:17:33 +0900

    SimpleDataLoggerプロジェクト追加

--- EnvironmentalSensor/EnvironmentalSensor/USB/Payloads/DataLongResponsePayload.cs
+++ EnvironmentalSensor/EnvironmentalSensor/USB/Payloads/DataLongResponsePayload.cs
@@ -34,41 +34,57 @@ namespace EnvironmentalSensor.USB.Payloads
         /// </summary>
         public Int16 AmbientLight { get; protected set; }
         /// <summary>
+        /// 気圧
         /// 範囲:300.000 to 1100.000
         /// 単位:0.001 hPa
         /// </summary>
         public Int32 BarometricPressure { get; protected set; }
         /// <summary>
+        /// 雑音
         /// 範囲:33.00 to 120.00
         /// 単位:0.01 dB
         /// </summary>
         public Int16 SoundNoise { get; protected set; }
         /// <summary>
-        /// equivalent Total Volatile Organic Compound
+        /// 総揮発性有機化学物量相当値（equivalent Total Volatile Organic Compounds）の略称
         /// 範囲:0 to 32767
         /// 単位:1 ppb
         /// </summary>
         public Int16 eTVOC { get; protected set; }
         /// <summary>
-        /// equivalent CO2
+        /// 二酸化炭素換算の数値(equivalent CO2)
         /// 範囲:400 to 32767
         /// 単位:1 ppm
         /// </summary>
         public Int16 eCO2 { get; protected set; }
         /// <summary>
+        /// 不快指数
         /// 夏の蒸し暑さを数量的に表現したもの．温度と湿度から換算する．
         /// 範囲:0.00 to 100.00
         /// 単位:0.01
         /// </summary>
         public Int16 DiscomfortIndex { get; protected set; }
         /// <summary>
+        /// 熱中症警戒度
         /// 熱中症の危険度を数量的に表現したもの．温度と湿度から換算する．
+        /// WEBサイト:熱中症に関する情報や不快指数はあくまで空調や体調管理の目安です。個人差、体調によって感じ方が大きく異なる場合があります。センサの出力により症状の発生有無を断定するものではありません。あくまで目安としてご利用ください。公的機関から発表される熱中症の警戒度とは一致しない場合があります。
         /// 範囲:-40.00 to 125.00
         /// 単位:0.01 degC
         /// </summary>
         public Int16 HeatStroke { get; protected set; }
-
+        /// <summary>
+        /// 振動情報
+        /// 0x00: NONE
+        /// 0x01: 振動中（地震判定中）during vibration(Earthquake judgment in progress)
+        /// 0x02: 地震中 during earthquake
+        /// </summary>
         public Byte VibrationInformation { get; protected set; }
+        /// <summary>
+        /// SI値：（スペクトル強度：Spectral Intensity）
+        /// WEBサイト:構造物に対する地震動の破壊エネルギーの大きさに相当します。SI値から震度相当値を算出でき、身近な環境の被害状況を把握できます
+        /// 範囲:0.0 to 6553.5
+        /// 単位:0.1 kine
+        /// </summary>
         public UInt16 SIValue { get; protected set; }
         /// <summary>
         /// ある区間の最大加速度値．水平 2 軸の加速度値を合成して換算する．
---
+++ EnvironmentalSensor/SimpleDataLogger/Program.cs
@@ -0,0 +1,178 @@
+﻿using EnvironmentalSensor.USB;
+using EnvironmentalSensor.USB.Payloads;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDataLogger
+{
+    class Program
+    {
+        static SerialPort serialPort = new SerialPort();
+        static StreamWriter logStream;
+
+        static void Main(string[] args)
+        {
+            var portName = "";
+            while (true)
+            {
+                Console.WriteLine($"ポート名を入力[COM1など]");
+                portName = Console.ReadLine();
+                Console.WriteLine($"{portName} でよろしいですか？[Y/N]");
+                var read = Console.ReadLine();
+                if (read.ToUpper() == "Y")
+                {
+                    break;
+                }
+            }
+            serialPort.DataReceived += SerialPort_DataReceived;
+            /// マニュアル:4.1. Communication specification
+            serialPort.BaudRate = 115200;
+            serialPort.DataBits = 8;
+            serialPort.StopBits = StopBits.One;
+            serialPort.Parity = Parity.None;
+            serialPort.Handshake = Handshake.None;// フロー制御：None
+            serialPort.PortName = portName;
+            serialPort.Open();
+            // 
+            using (logStream = new StreamWriter("log.txt", true, Encoding.UTF8))
+            {
+                LogHeader();
+                EnvironmentalSensorCommunication();
+                Console.WriteLine("終了するにはなにかキーを入力");
+                Console.ReadLine();
+            }
+            // 終了
+            serialPort.Close();
+        }
+
+        static async void EnvironmentalSensorCommunication()
+        {
+            while (true)
+            {
+                if (serialPort.IsOpen)
+                {
+                    var payload = new LatestDataLongCommandPayload();
+                    var frame = new Frame(payload);
+                    var buffer = frame.ToBytes();
+                    serialPort.Write(buffer, 0, buffer.Length);
+                }
+                else
+                {
+                    Console.WriteLine("閉じてる");
+                }
+                await Task.Delay(1000);
+            }
+        }
+
+        /// <summary>
+        /// データが受信された
+        /// </summary>
+        private static void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var now = DateTime.Now;
+            byte[] buffer = new byte[1024];
+            var readSize = serialPort.Read(buffer, 0, buffer.Length);
+            Console.WriteLine("DataReceived");
+            Console.WriteLine($"readSize={readSize}");
+            if (readSize > 0)
+            {
+                try
+                {
+                    var frame = new Frame(buffer, 0, readSize);
+
+                    if (frame.Payload is ErrorResponsePayload)
+                    {
+                        var payload = frame.Payload as ErrorResponsePayload;
+                        Log(now, payload);
+                    }
+                    else if (frame.Payload is LatestDataLongResponsePayload)
+                    {
+                        var payload = frame.Payload as LatestDataLongResponsePayload;
+                        Log(now, payload);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        static void LogHeader()
+        {
+            logStream.WriteLine(
+                $"日時," +
+                $"SequenceNumber," +
+                $"気温[℃]," +
+                $"相対湿度[％]," +
+                $"照度[ルクス]," +
+                $"気圧[hPa]," +
+                $"雑音[dB]," +
+                $"総揮発性有機化学物量相当値[ppd]," +
+                $"二酸化炭素換算の数値[ppm]," +
+                $"不快指数," +
+                $"熱中症警戒度[℃]," +
+                $"振動情報," +
+                $"スペクトル強度[kine]," +
+                $"PGA," +
+                $"SeismicIntensity," +
+                $"TemperatureFlag," +
+                $"RelativeHumidityFlag," +
+                $"AmbientLightFlag," +
+                $"BarometricPressureFlag," +
+                $"SoundNoiseFlag," +
+                $"eTVOCFlag," +
+                $"eCO2Flag," +
+                $"DiscomfortIndexFlag," +
+                $"HeatStrokeFlag," +
+                $"SIValueFlag," +
+                $"PGAFlag," +
+                $"SeismicIntensityFlag,"
+                );
+        }
+        static void Log(DateTime now, ErrorResponsePayload payload)
+        {
+            logStream.WriteLine(
+                $"{now.ToString("yyyy/MM/dd hh:mm:ss")}," +
+                $"Error," +
+                $"{payload.Code},"
+                );
+        }
+        static void Log(DateTime now, LatestDataLongResponsePayload payload)
+        {
+            logStream.WriteLine(
+                $"{now.ToString("yyyy/MM/dd hh:mm:ss")}," +
+                $"{payload.SequenceNumber}," +
+                $"{payload.Temperature * 0.01}," +
+                $"{payload.RelativeHumidity * 0.01}" +
+                $"{payload.AmbientLight}," +
+                $"{payload.BarometricPressure * 0.001}," +
+                $"{payload.SoundNoise}," +
+                $"{payload.eTVOC}," +
+                $"{payload.eCO2}," +
+                $"{payload.DiscomfortIndex}," +
+                $"{payload.HeatStroke}," +
+                $"{payload.VibrationInformation}," +
+                $"{payload.SIValue}," +
+                $"{payload.PGA}," +
+                $"{payload.SeismicIntensity}," +
+                $"{payload.TemperatureFlag}," +
+                $"{payload.RelativeHumidityFlag}," +
+                $"{payload.AmbientLightFlag}," +
+                $"{payload.BarometricPressureFlag}," +
+                $"{payload.SoundNoiseFlag}," +
+                $"{payload.eTVOCFlag}," +
+                $"{payload.eCO2Flag}," +
+                $"{payload.DiscomfortIndexFlag}," +
+                $"{payload.HeatStrokeFlag}," +
+                $"{payload.SIValueFlag}," +
+                $"{payload.PGAFlag}," +
+                $"{payload.SeismicIntensityFlag},"
+                );
+        }
+    }
+}
