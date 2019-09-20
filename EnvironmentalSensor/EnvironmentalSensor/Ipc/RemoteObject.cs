dbff176ddd8be81bf70f25241c247e89921f853a
笠浪貴大 <dev.kacha@gmail.com>
2019-09-20 22:27:15 +0900

    ペイロードの記録をListに変更

--- EnvironmentalSensor/EnvironmentalSensor/Ipc/RemoteObject.cs
+++ EnvironmentalSensor/EnvironmentalSensor/Ipc/RemoteObject.cs
@@ -1,11 +1,16 @@
 ﻿using EnvironmentalSensor.USB;
 using EnvironmentalSensor.USB.Payloads;
 using System;
+using System.Collections.Generic;
 
 namespace EnvironmentalSensor.Ipc
 {
     public class RemoteObject : MarshalByRefObject
     {
+        /// <summary>
+        /// Payloadsの最大数
+        /// </summary>
+        const int PayloadsMaxCount = 1024;
         /// <summary>
         /// 情報を設定した日時
         /// </summary>
@@ -17,13 +22,12 @@ namespace EnvironmentalSensor.Ipc
         /// <summary>
         /// 最新のPayloadsのインデックス
         /// </summary>
-        public int Index { get; protected set; }
+        public int LatestIndex { get; protected set; }
         /// <summary>
         /// センサーから受信した情報
-        /// <para>インデックスを回して格納される</para>
-        /// <para>インデックスが最後になったら最初に戻る</para>
+        /// <para>最大値に達したら古いものから削除される</para>
         /// </summary>
-        public FramePayload[] Payloads { get; protected set; } = new FramePayload[256];
+        public List<FramePayload> Payloads { get; protected set; } = new List<FramePayload>();
         /// <summary>
         /// 情報の更新が完了しているならtrue
         /// <para>情報の更新直前にfalseに設定される</para>
@@ -38,9 +42,13 @@ namespace EnvironmentalSensor.Ipc
             UpdateCompleted = false;
 
             TimeStampTicks = DateTime.Now.Ticks;
-            Index++;
-            if (Index >= Payloads.Length) { Index = 0; }
-            Payloads[Index] = payload;
+
+            Payloads.Add(payload);
+            if (Payloads.Count > PayloadsMaxCount)
+            {
+                Payloads.RemoveAt(0);
+            }
+            LatestIndex = Payloads.Count - 1;
 
             UpdateCompleted = true;
         }
